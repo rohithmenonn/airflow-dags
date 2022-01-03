7e9c8620c37e5d6ef00200f112b0eae99c25e03d
Rohith Menon <rohith.menon@karkinos.in>
2022-01-03 19:00:00 +0530

    my first sample dag

--- hello_world_dag_rm.py
+++ hello_world_dag_rm.py
@@ -1,16 +1,16 @@
-# import datetime as dt
-# from airflow import DAG
-# from airflow.operators.bash import BashOperator
+import datetime as dt
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 
-# default_args = {
-#     'owner': 'me',
-#     'start_date': dt.datetime(2021, 12, 31)
-# }
+default_args = {
+    'owner': 'me',
+    'start_date': dt.datetime(2021, 12, 31)
+}
 
-# with DAG('airflow_dag_test_rm_v01', default_args) as dag:
-#     print_hello = BashOperator(task_id='print_hello',
-#         bash_command='echo hello')
-#     print_world = BashOperator(task_id='print_world',
-#         bash_command='echo world')
+with DAG('airflow_dag_test_rm_v01', default_args) as dag:
+    print_hello = BashOperator(task_id='print_hello',
+        bash_command='echo hello')
+    print_world = BashOperator(task_id='print_world',
+        bash_command='echo world')
 
-# print_hello >> print_world
+print_hello >> print_world
